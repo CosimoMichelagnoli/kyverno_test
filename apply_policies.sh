aaf86f4c856f9456ccc7770b3ec7bfcb2c81a806
GRAZIA DONGHIA s284633 <s284633@studenti.polito.it>
2022-05-04 09:57:39 +0200

    Finished kind tests

--- apply_policies.sh
+++ apply_policies.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/zsh
-echo "Applying kyverno policies to cluster $(sudo kind get clusters)..."
-
-for name in $( kubectl get ns --no-headers -o custom-columns=":metadata.name" ); do
-    echo $name
+echo "Applying kyverno policies to sandbox namespaces in cluster...\n"
+for name in $( kubectl get ns --no-headers -o custom-columns=":metadata.name"|grep sandbox ); do
+    echo "Namespace $name"
+    kubectl apply -f policies/ --namespace ${name}
+    echo "\n"
 done
+echo "Done"
